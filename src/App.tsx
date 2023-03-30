dd91d346b15d51b7c90b0347240c33a359e0137a
Jannatul Fardus Muslim <jannatul.fardus@hotmail.com>
2023-03-30 12:18:40 +0100

    Add daynamic page heading

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 import { PlatformSelector } from "./Components/PlatformSelector";
 import { Platform } from "./Hooks/useGames";
 import { SortSelected } from "./Components/SortSelected";
+import GameHeading from "./Components/GameHeading";
 
 export interface GameQuery{
   genre: Genre | null;
@@ -40,12 +41,15 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
-        <Flex paddingLeft={2} marginBottom={5}>
-          <Box marginRight={5}>
-          <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) => setGameQuery({ ...gameQuery,platform})}/>
-          </Box>
-          <SortSelected sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({...gameQuery, sortOrder})} />
-        </Flex>
+        <Box paddingLeft={2} >
+          <GameHeading gameQuery={gameQuery} />
+          <Flex marginBottom={5}>
+            <Box marginRight={5}>
+            <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) => setGameQuery({ ...gameQuery,platform})}/>
+            </Box>
+            <SortSelected sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({...gameQuery, sortOrder})} />
+          </Flex>
+        </Box>
         <GameGrid gameQuery={gameQuery}/>
       </GridItem>
     </Grid>
